51fde0c2b5bf1a2dce5be04d864bfd218b266d9e
moses mccabe <moses.peace.mccabe@gmail.com>
2021-11-10 16:40:40 -0800

    Initial Commit

---
+++ H4ker New/H4ker_NewApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4ker_NewApp.swift
+//  H4ker New
+//
+//  Created by Moses Mccabe on 11/10/21.
+//
+
+import SwiftUI
+
+@main
+struct H4ker_NewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
